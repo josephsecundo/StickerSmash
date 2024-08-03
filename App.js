b75cbe8eca33110bb8a89a8b5ff95025ab31d080
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-08-02 22:47:16 -0300

    Primeira aula android

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import Button from './.expo/components/Button';
+import ImageViewer from './.expo/components/ImageViewer';
+
+const PlaceholderImage = require('./assets/images/background-image.png')
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer PlaceholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+
+  footerContainer: {
+    flex: 1 / 3,
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  
 });
